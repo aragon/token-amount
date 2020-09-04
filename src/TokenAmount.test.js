643683f9481f02db93c913d85e358a9a54e3788e
Enrique Ortiz <hi@enriqueortiz.dev>
2020-09-04 15:56:02 -0400

    Add more test to further bulletproof everything

--- src/TokenAmount.test.js
+++ src/TokenAmount.test.js
@@ -26,6 +26,48 @@ describe('formatTokenAmount()', () => {
     const amount3 = new TokenAmount('1', 0)
 
     expect(amount3.convert('0.5', 2).value.toString()).toEqual('50')
+
+    const amount4 = new TokenAmount(oneEth, 18, { symbol: 'ANT' })
+
+    // 1 ANT to ANJ price on 04/09/20 (DD/MM/YY)
+    expect(
+      amount4.convert('70.63271216490210467', 18).value.toString()
+    ).toEqual('70632712164902104670')
+
+    const amount5 = new TokenAmount('2000000000000000000', 18, {
+      symbol: 'ANT',
+    })
+
+    expect(
+      amount5.convert('70.63271216490210467', 18).value.toString()
+    ).toEqual('141265424329804209340')
+
+    // 1 ETH -> USDC (18 decimals to 6 decimals conversion)
+    const amount6 = new TokenAmount(oneEth, 18, { symbol: 'ANT' })
+
+    expect(amount6.convert('384.049', 6).value.toString()).toEqual('384049000')
+
+    const amount7 = new TokenAmount(oneEth, 18, { symbol: 'ANT' })
+
+    expect(amount7.convert('102', 1).value.toString()).toEqual('1020')
+
+    // Tiny rate (ANT to WBTC)
+    // 0.00537237 WBTC per 1 ANT
+    // Also 18 decimals -> 8 decimals
+    const amount8 = new TokenAmount(oneEth, 18, {
+      symbol: 'ANT',
+    })
+
+    expect(amount8.convert('0.00053727', 8).value.toString()).toEqual('53727')
+
+    // WBTC to ANT (reverse case)
+    const amount9 = new TokenAmount('100000000', 8, {
+      symbol: 'WBTC',
+    })
+
+    expect(amount9.convert('1823.17', 18).value.toString()).toEqual(
+      '1823170000000000000000'
+    )
   })
   test('should format correctly', () => {
     const amount = new TokenAmount(BigInt('9388295879707883945'), 18)
