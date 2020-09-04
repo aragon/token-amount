61d450dc46fdc27cd98263fff8cd77554cff2748
Pierre Bertet <hello@pierre.world>
2020-09-04 01:50:54 +0100

    Tests: coding style
    - Use BigInt (available in Node).
    - Pass numbers directly when possible.

--- src/convert.test.js
+++ src/convert.test.js
@@ -1,51 +1,31 @@
-import JSBI from 'jsbi'
 import { getConvertedAmount } from './convert'
 
-const ONE_ETH = JSBI.BigInt('1000000000000000000')
+const ONE_ETH = 1000000000000000000n
 
 describe('getConvertedAmount tests', () => {
   test('Converts amounts correctly', () => {
-    expect(getConvertedAmount(JSBI.BigInt('1'), 1, 1, 1).toString()).toEqual(
-      '1'
-    )
+    expect(getConvertedAmount(1, 1, 1, 1).toString()).toEqual('1')
     expect(getConvertedAmount(ONE_ETH, 18, 0.5, 4).toString()).toEqual('5000')
-    expect(getConvertedAmount(JSBI.BigInt('1'), 0, 0.5, 2).toString()).toEqual(
-      '50'
-    )
-    expect(getConvertedAmount(JSBI.BigInt('1'), 0, 0.25, 2).toString()).toEqual(
-      '25'
-    )
-    expect(
-      getConvertedAmount(JSBI.BigInt('1'), 0, 0.125, 3).toString()
-    ).toEqual('125')
-
-    expect(getConvertedAmount(JSBI.BigInt('100'), 0, 50, 0).toString()).toEqual(
-      '5000'
-    )
+    expect(getConvertedAmount(1, 0, 0.5, 2).toString()).toEqual('50')
+    expect(getConvertedAmount(1, 0, 0.25, 2).toString()).toEqual('25')
+    expect(getConvertedAmount(1, 0, 0.125, 3).toString()).toEqual('125')
+    expect(getConvertedAmount(100, 0, 50, 0).toString()).toEqual('5000')
     expect(getConvertedAmount(ONE_ETH, 18, 400, 2).toString()).toEqual('40000')
   })
 
   test('Converts amounts correctly with rates being strings', () => {
-    expect(getConvertedAmount(JSBI.BigInt('1'), 1, '1', 1).toString()).toEqual(
-      '1'
-    )
+    expect(getConvertedAmount(1, 1, '1', 1).toString()).toEqual('1')
     expect(getConvertedAmount(ONE_ETH, 18, '0.5', 4).toString()).toEqual('5000')
-    expect(
-      getConvertedAmount(JSBI.BigInt('1'), 0, '0.5', 2).toString()
-    ).toEqual('50')
-    expect(
-      getConvertedAmount(JSBI.BigInt('1'), 0, '0.25', 2).toString()
-    ).toEqual('25')
-    expect(
-      getConvertedAmount(JSBI.BigInt('1'), 0, '0.125', 3).toString()
-    ).toEqual('125')
-
-    expect(
-      getConvertedAmount(JSBI.BigInt('100'), 0, '50', 0).toString()
-    ).toEqual('5000')
+    expect(getConvertedAmount(1, 0, '0.5', 2).toString()).toEqual('50')
+    expect(getConvertedAmount(1, 0, '0.25', 2).toString()).toEqual('25')
+    expect(getConvertedAmount(1, 0, '0.125', 3).toString()).toEqual('125')
+    expect(getConvertedAmount(100, 0, '50', 0).toString()).toEqual('5000')
     expect(getConvertedAmount(ONE_ETH, 18, '400', 2).toString()).toEqual(
       '40000'
     )
+    expect(
+      getConvertedAmount(4000n * ONE_ETH, 18, '400', 2).toString()
+    ).toEqual('160000000')
   })
 
   test('rounds truncated decimals properly', () => {
