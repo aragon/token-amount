a8086b9cd6c5b50798c4540c68c6f44e91ae99cf
Andy Hook <hello@andy-hook.co.uk>
2020-11-17 12:27:16 +0000

    divideRoundBigInt: Support BigIntish

--- src/math.ts
+++ src/math.ts
@@ -1,10 +1,18 @@
+import { BigIntish } from './types'
+
 /**
  * Divide and round two big integers.
  *
- * @param {bigint} dividend Integer to be divided + rounded
- * @param {bigint} divisor  Divisor
- * @returns {bigint}
+ * @param {BigInt|string|number} dividend Integer to be divided + rounded
+ * @param {BigInt|string|number} divisor  Divisor
+ * @returns {BigInt}
  */
-export function divideRoundBigInt(dividend: bigint, divisor: bigint): bigint {
-  return (dividend + divisor / BigInt(2)) / divisor
+export function divideRoundBigInt(
+  dividend: BigIntish,
+  divisor: BigIntish
+): bigint {
+  const parsedDividend = BigInt(String(dividend))
+  const parsedDivisor = BigInt(String(divisor))
+
+  return (parsedDividend + parsedDivisor / BigInt(2)) / parsedDivisor
 }
